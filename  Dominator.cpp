f47ca001d4e23f379d6a1f194ef2ebc3385a67aa
Jimin Park <jpark1607@gmail.com>
2021-09-01 00:36:29 +0900

    Dominator Complete

---
+++  Dominator.cpp
@@ -0,0 +1,37 @@
+#include <algorithm>
+
+int
+solution(vector<int> &A)
+{
+    int i, j;
+    int len = A.size();
+    int cnt = 1;
+    int dom = (len / 2) + 1;
+    vector<int> cp;
+
+    if (len == 0 || len == 1)
+        return (len - 1);
+
+    cp.assign(A.begin(), A.end());
+    sort(A.begin(), A.end());
+
+    for (i = 0; i < len; i++) {
+        if (A[i] != A[i + 1]) {
+            cnt = 1;
+            if (len - i - 1 < dom)
+                return -1;
+            else
+                continue;
+        }
+        else
+            cnt++;
+
+        if (cnt == dom) {
+            for (j = 0; j < dom; j++) {
+                if (cp[j] == A[i])
+                    return j;
+            }
+        }
+    }
+    return -1;
+}
