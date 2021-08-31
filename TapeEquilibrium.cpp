012e63a2880e8402c4eab7675a34e307e9e813f4
Jimin Park <jpark1607@gmail.com>
2021-08-31 23:44:21 +0900

    TapeEquilibrium Complete

---
+++ TapeEquilibrium.cpp
@@ -0,0 +1,21 @@
+#include <cstdlib>
+#include <numeric>
+
+int
+solution(vector<int> &A)
+{
+    int sum = accumulate(A.begin(), A.end(), 0);
+    int min = 999999999;
+    int size = A.size();
+    int res;
+
+
+    for (int i = 0; i < size - 1; i++) {
+        sum -= A[i] * 2;
+        res = abs(sum);
+        if (min > res) min = res;
+        if (min == 0) return 0;
+    }
+
+    return min;
+}
