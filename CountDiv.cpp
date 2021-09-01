cb8b3a258905bc4618a10ac6e12d9c82593e79b1
Jimin Park <jpark1607@gmail.com>
2021-09-01 23:47:17 +0900

    CountDiv on going(37)

---
+++ CountDiv.cpp
@@ -0,0 +1,18 @@
+// v1 : 37
+int
+solution(int A, int B, int K)
+{
+    long alpha = ((A + (K - 1)) / K) * K;
+    long beta = (((B + (K - 1)) / K) - 1) * K;
+
+    if (alpha > beta)
+        return 0;
+    else if (alpha == beta) {
+        if (alpha % K == 0)
+            return 1;
+        else
+            return 0;
+    }
+    else
+        return ((((int)(beta - alpha)) / K) + 1);
+}
