13ad7e5dae1446b794e44219abc0217368e4a8ea
Jimin Park <jpark1607@gmail.com>
2021-09-02 00:08:21 +0900

    CountFactors Complete

--- CountFactors.cpp
+++ CountFactors.cpp
@@ -1,16 +1,35 @@
-// v1 : 85
+/* v2 */
 #include <cmath>
-#include <vector>
 
 int
 solution(int N)
 {
     int i;
+    int sq = sqrt(N);
+    int cnt = 0;
+
+    for (i = 1; i <= sq; i++) {
+        if (N % i == 0)
+            cnt += 2;
+    }
+
+    if (sq * sq == N)
+        cnt--;
+
+    return cnt;
+}
+
+/* v1 : 85
+ * performance problem */
+#include <vector>
+
+int
+solution(int N)
+{
     vector<int> V;
     int K = N;
-    int cnt;
-    int total = 1;
-    int size;
+    int cnt, size, total = 1;
+    int i;
 
     for (i = 2; i <= N; i++) {
         cnt = 0;
