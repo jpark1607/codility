7bc2e5fa50c0af4300fd57270dceca9988986a7a
Jimin Park <jpark1607@gmail.com>
2021-09-03 21:56:56 +0900

    MaxCounters Complete

--- MaxCounters.cpp
+++ MaxCounters.cpp
@@ -1,3 +1,36 @@
+// v2 : 100
+vector<int>
+solution(int N, vector<int> &A)
+{
+    vector<int> v(N, 0);
+    int size = A.size();
+    int max = 0;
+    int gap = 0;
+    int i;
+
+    for (i = 0; i < size; i++) {
+        if (A[i] <= N) {
+            if (v[A[i] - 1] < gap)
+                v[A[i] - 1] = gap;
+
+            v[A[i] - 1] += 1;
+
+            if (v[A[i] - 1] > max)
+                max = v[A[i] - 1];
+        }
+        else {
+            gap = max;
+        }
+    }
+
+    for (i = 0; i < N; i++) {
+        if (v[i] < gap)
+            v[i] = gap;
+    }
+
+    return v;
+}
+
 // v1 : 77
 vector<int>
 solution(int N, vector<int> &A)
