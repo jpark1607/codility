f9b264b12fd4caeaed8ca185e991afa546edde3f
Jimin Park <jpark1607@gmail.com>
2021-09-05 13:23:31 +0900

    MinAvgTwoSlice Complete

--- MinAvgTwoSlice.cpp
+++ MinAvgTwoSlice.cpp
@@ -1,3 +1,33 @@
+/* v2 : 100
+ * searched : the result slice array only can be 2 or 3. */
+int
+solution(vector<int> &A)
+{
+    int size = A.size();
+    int i;
+    double min = ((double)A[0] + (double)A[1]) / 2;
+    int idx = 0;
+    double avg;
+
+
+    for (i = 2; i < size; i++) {
+        avg = ((double)A[i - 2] + (double)A[i - 1] + (double)A[i]) / 3;
+        if (min > avg) {
+            idx = i - 2;
+            min = avg;
+        }
+
+        avg = ((double)A[i - 1] + (double)A[i]) / 2;
+        if (min > avg) {
+            idx = i - 1;
+            min = avg;
+        }
+    }
+
+    return idx;
+}
+
+
 /* v1 : 60
  * performance problem */
 int
