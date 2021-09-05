552c00097ea1ec908fdafe79f1ea72c0beae117e
Jimin Park <jpark1607@gmail.com>
2021-09-06 00:59:03 +0900

    GenomicRangeQuery Complete

--- GenomicRangeQuery.cpp
+++ GenomicRangeQuery.cpp
@@ -1,3 +1,67 @@
+/* v3 : 100
+ * searched : make arrays for each letter, cumulative values */
+vector<int>
+solution(string &S, vector<int> &P, vector<int> &Q)
+{
+    int M = P.size();
+    vector<int> res;
+    vector<int> aA, aC, aG;
+    int A = 0, C = 0, G = 0;
+    int size = S.size();
+    int i;
+
+    aA.push_back(0);
+    aC.push_back(0);
+    aG.push_back(0);
+
+    for (i = 0; i < size; i++) {
+        switch (S[i]) {
+        case 'A':
+            A++;
+            break;
+        case 'C':
+            C++;
+            break;
+        case 'G':
+            G++;
+            break;
+        }
+        aA.push_back(A);
+        aC.push_back(C);
+        aG.push_back(G);
+    }
+
+    for (i = 0; i < M; i++) {
+        if (P[i] == Q[i]) {
+            switch (S[P[i]]) {
+            case 'A':
+                res.push_back(1);
+                break;
+            case 'C':
+                res.push_back(2);
+                break;
+            case 'G':
+                res.push_back(3);
+                break;
+            case 'T':
+                res.push_back(4);
+                break;
+            }
+        }
+        else if (aA[Q[i] + 1] - aA[P[i]] != 0)
+            res.push_back(1);
+        else if (aC[Q[i] + 1] - aC[P[i]] != 0)
+            res.push_back(2);
+        else if (aG[Q[i] + 1] - aG[P[i]] != 0)
+            res.push_back(3);
+        else
+            res.push_back(4);
+    }
+
+    return res;
+}
+
+
 /* v2 : 87
  * performance for extreme_large */
 vector<int>
