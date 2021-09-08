59bac07e30bc419d1d309e69b6253936ce7a2dc0
Jimin Park <jpark1607@gmail.com>
2021-09-08 22:45:25 +0900

    AbsDistinct Complete

--- AbsDistinct.cpp
+++ AbsDistinct.cpp
@@ -1,3 +1,74 @@
+/* v2 : 100 */
+#include <cstdlib>
+#ifndef _WIN32
+#include <bits/stdc++.h>
+#else
+#include <limits>
+#endif /* _WIN32, for INT_MIN */
+
+int
+solution(vector<int> &A)
+{
+    int size = A.size();
+    int cnt = 0;
+    int last_val = -1;
+    bool finished = false;
+    int i, j;
+
+    i = 0;
+    j = size - 1;
+
+    /* 1 or 2 units */
+    if (size <= 2)
+        return (A[i] == A[j] ? 1 : 2);
+
+    /* if INT_MIN, abs() will not return properly */
+    while (A[i] == INT_MIN)
+        i++;
+    if (i > 0)
+        cnt++;
+
+    while (1) {
+        while ((abs(A[i]) > abs(A[j])) && !finished) {
+            if (last_val != abs(A[i])) {
+                last_val = abs(A[i]);
+                cnt++;
+            }
+
+            i++;
+            if (i > j)
+                finished = true;
+        }
+        while ((abs(A[i]) < abs(A[j])) && !finished) {
+            if (last_val != abs(A[j])) {
+                last_val = abs(A[j]);
+                cnt++;
+            }
+
+            j--;
+            if (i > j)
+                finished = true;
+        }
+        while ((abs(A[i]) == abs(A[j])) && !finished) {
+            if (last_val != abs(A[i])) {
+                last_val = abs(A[i]);
+                cnt++;
+            }
+
+            i++;
+            j--;
+            if (i > j)
+                finished = true;
+        }
+
+        if (finished)
+            break;
+    }
+
+    return cnt;
+}
+
+
 /* v1 : 57
  * wrong answer */
 #include <cstdlib>
