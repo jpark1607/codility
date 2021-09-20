f45c915b5b0e22b78be989927bdd1399aa7acb71
jpark1607 <jpark1607@gmail.com>
2021-09-20 16:29:07 +0900

    Distinct Complete with Comments

--- Distinct.cpp
+++ Distinct.cpp
@@ -1,3 +1,15 @@
+/* Lesson 6.1
+ * 1. 문제:
+ *   distinct value의 수를 구하여라.
+ *
+ * 2. 아이디어:
+ *   bool type array 만들어서 distinct한 게 나오면 true로 바꿔주고, count를 올려준다.
+ *   이미 true인 상태면 지나간다.
+ *
+ * 3. 코드:
+ *   [A] value의 distinct를 체크할 bool type array를 만든다.
+ *   [B] 특정 value에 해당하는 bool 값이 false이면, true로 바꿔주면서 count를 올려준다.
+ * */
 int
 solution(vector<int> &A)
 {
@@ -8,9 +20,11 @@ solution(vector<int> &A)
     if (size < 2)
         return size;
 
+    /* [A] */
     arr = new bool[2000001] {false};
 
     for (int i = 0; i < size; i++) {
+        /* [B] */
         if (arr[A[i] + 1000000] == false) {
             arr[A[i] + 1000000] = true;
             cnt++;
