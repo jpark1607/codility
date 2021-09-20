0aaed264842101f7fe13ef6a39511580e091f10b
jpark1607 <jpark1607@gmail.com>
2021-09-20 16:38:34 +0900

    Brackets Complete with Comments

--- Brackets.cpp
+++ Brackets.cpp
@@ -1,5 +1,18 @@
 #include <stack>
 
+/* Lesson 7.1
+ * 1. 문제:
+ *   String S가 properly nested(정상 중첩?)인지 확인하여라.
+ *   예를 들어 "{[()()]}"는 properly nested이고, "([)()]"는 아니다.
+ *
+ * 2. 아이디어:
+ *   stack을 활용한다.
+ *   open bracket('{', '[', '(')은 stack에 바로 넣고, close bracket('}', ']', ')')은 stack을 pop하면서 자기 짝이 맞는지 확인해본다.
+ *
+ * 3. 코드:
+ *   [A] open bracket은 stack에 push한다.
+ *   [B] close bracket은 stack에서 pop하면서 자기 짝이 맞는지 확인한다.
+ * */
 int
 solution(string &S)
 {
@@ -7,11 +20,13 @@ solution(string &S)
 
     for (string::iterator it = S.begin(); it != S.end(); ++it) {
         switch (*it) {
+        /* [A] */
         case '(':
         case '{':
         case '[':
             st.push(*it);
             break;
+        /* [B] */
         case ')':
             if (st.top() != '(')
                 return 0;
