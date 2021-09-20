485459ddcc105c18a2dbd5fb4771bedda5f57d95
jpark1607 <jpark1607@gmail.com>
2021-09-20 16:40:17 +0900

    Nesting Complete with Comments

--- Nesting.cpp
+++ Nesting.cpp
@@ -1,6 +1,18 @@
-// Same as Bracket..?
 #include <stack>
 
+/* Lesson 7.3
+ * 1. 문제:
+ *   String S가 properly nested(정상 중첩?)인지 확인하여라.
+ *   예를 들어 "(()(())())"는 properly nested이고, "())"는 아니다.
+ *
+ * 2. 아이디어:
+ *   stack을 활용한다.
+ *   open bracket('(')은 stack에 바로 넣고, close bracket(')')은 stack을 pop하면서 자기 짝이 맞는지 확인해본다.
+ *
+ * 3. 코드:
+ *   [A] open bracket은 stack에 push한다.
+ *   [B] close bracket은 stack에서 pop하면서 자기 짝이 맞는지 확인한다.
+ * */
 int
 solution(string &S)
 {
