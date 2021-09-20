1f7039c00fbb9f9251cdc29ef207b35cd5488d92
Jimin Park <jpark1607@gmail.com>
2021-09-20 20:22:22 +0900

    CountFactors Complete with Comments

--- CountFactors.cpp
+++ CountFactors.cpp
@@ -36,35 +36,3 @@ solution(int N)
 
     return cnt;
 }
-
-/* v1 : 85
- * performance problem */
-#include <vector>
-
-int
-solution(int N)
-{
-    vector<int> V;
-    int K = N;
-    int cnt, size, total = 1;
-    int i;
-
-    for (i = 2; i <= N; i++) {
-        cnt = 0;
-
-        while (K % i == 0) {
-            K /= i;
-            cnt++;
-        }
-        if (cnt > 0)
-            V.push_back(cnt + 1);
-        if (K == 1)
-            break;
-    }
-
-    size = V.size();
-    for (i = 0; i < size; i++)
-        total *= V[i];
-
-    return total;
-}
